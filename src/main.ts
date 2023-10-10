5b6cae8f99ebd2d9934382498e47f71ce197af93
PiusLucky <luckypius50@gmail.com>
2023-10-10 14:42:23 +0100

    chore: deploying to cyclic

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { movieSeeder } from 'seeders/movieSeeder';
 import { GeneratorModule } from './generator/generator.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(GeneratorModule);
   // Seed the database here
   await movieSeeder();
-  await app.listen(3000);
+  app.listen(3000, () => {
+    Logger.log(`The app has started on PORT 3000`);
+  });
 }
 bootstrap();
